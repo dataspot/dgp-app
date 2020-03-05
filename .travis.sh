db728a03db26388fe890c6d948bcd3c450350ccd
Adam Kariv <adam.kariv@gmail.com>
2020-03-05 16:32:47 +0200

    Travis

--- .travis.sh
+++ .travis.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DOCKER_IMAGE=akariv/airflow-config-ui
+DOCKER_IMAGE=akariv/dgp-app
 
 if [ "${1}" == "install" ]; then
     curl -L https://raw.githubusercontent.com/OriHoch/travis-ci-operator/master/travis_ci_operator.sh > $HOME/bin/travis_ci_operator.sh &&\
