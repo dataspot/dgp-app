cd027e1871a7eb4d27aa28e8d0573ed0f4c1723e
Adam Kariv <adam.kariv@gmail.com>
2021-01-05 23:35:41 +0200

    Fix build

--- ui/projects/etl-server/src/lib/dashboard/dashboard/dashboard.component.ts
+++ ui/projects/etl-server/src/lib/dashboard/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'dgp-oauth2-ng';
+import { first } from 'rxjs/operators';
+import { ApiService } from '../../api.service';
 import { RolesService } from '../../roles.service';
 
 @Component({
@@ -10,13 +12,19 @@ import { RolesService } from '../../roles.service';
 export class DashboardComponent implements OnInit {
 
   profile = null;
+  configuration: any;
 
-  constructor(public auth: AuthService, public roles: RolesService) {
+  constructor(public auth: AuthService, private api: ApiService, public roles: RolesService) {
     this.auth.getUser().subscribe((user) => {
       if (user) {
         this.profile = user.profile;
       }
     });
+    this.api.configuration.pipe(first()).subscribe(
+      (configuration) => {
+        this.configuration = configuration;
+      }
+    )
   }
 
   ngOnInit() {
