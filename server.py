9698e8995b42f7952acb58c7684d69e87db779e7
Adam Kariv <adam.kariv@gmail.com>
2021-06-15 13:55:58 +0300

    reduce level for log print

--- server.py
+++ server.py
@@ -143,4 +143,4 @@ else:
     gunicorn_error_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers.extend(gunicorn_error_logger.handlers)
     app.logger.setLevel(logging.DEBUG)
-    app.logger.error('DGP-APP STARTING')
+    app.logger.info('DGP-APP STARTING')
